687bd4f820232aa4d3f0bd02348c9e4d74f0c9ab
yar-ua <yar.ua.1985@gmail.com>
2019-12-18 21:54:33 +0200

    add test DB script

---
+++ createDb.js
@@ -0,0 +1,35 @@
+var mongoose = require('./libs/mongoose');
+var async = require('async');
+
+async.series([
+  open,
+  dropDatabase,
+  createUsers
+], function(err, results){
+  console.log(arguments);
+  mongoose.disconnect();
+});
+
+function open(callback){
+  mongoose.connection.on('open', callback);
+};
+
+function dropDatabase(callback){
+  var db = mongoose.connection.db;
+  db.dropDatabase(callback);
+};
+
+function createUsers(callback){
+  require('./models/user');
+
+  var users = [
+    {username: 'Вася', password: 'supervasya'},
+    {username: 'Петя', password: '123'},
+    {username: 'admin', password: 'thetruehero'}
+  ];
+  async.each(users, function(userData, callback){
+    var user = new mongoose.models.User(userData);
+    user.save(callback);
+  }, callback);
+};
+
