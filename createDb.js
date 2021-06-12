1d9a172fe867bf94f16350cc9d6fec64fc97a09b
Yar-ua <yar.ua.1985@gmail.com>
2021-06-12 23:24:59 +0300

    add mongo and mongoose

--- createDb.js
+++ createDb.js
@@ -1,14 +1,69 @@
-var User = require('models/user').User;
+var User = require('./models/user').User;
 
 var user = new User({
-  username: "Tester2",
+  username: "Tester",
   password: "secret"
 });
 
-user.save(function(err, user, affected) {
+user.save(function(err, user, affected){
   if (err) throw err;
 
-  User.findOne({username: "Tester"}, function(err, tester) {
+  User.findOne({username: "Tester"}, function(err, tester){
     console.log(tester);
-  });
-});
+  })
+  console.log(arguments);
+})
+
+
+// user.save();
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/node_chat');
+
+// const catSchema = new mongoose.Schema({
+//   name: String
+// });
+
+// const Cat = mongoose.model('Cat', catSchema);
+
+// var kitty = new Cat({
+//   name: "Tom"
+// });
+
+// console.log(kitty);
+
+// kitty.save(function(err, kitty, affected){
+//   console.log(arguments);
+// })
+
+
+
+
+
+// const { MongoClient } = require('mongodb');
+// const url = 'mongodb://localhost:27017';
+// const client = new MongoClient(url);
+
+// const dbName = 'node_chat';
+
+// client.connect();
+// console.log('Connected successfully to server');
+// const db = client.db(dbName);
+// const collection = db.collection('test_insert');
+// collection.insertOne({a:2}, function(err, docs){
+//   collection.count(function(err, count){
+//     console.log("conunt = %s", count);
+//   });
+
+//   collection.find().toArray(function(err, results){
+//     console.dir(results);
+//     client.close();
+//   })
+// })
+
+// console.log('done.');
